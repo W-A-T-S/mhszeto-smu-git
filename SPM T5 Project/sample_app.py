c3cde30d2deee949c231aace739e5d3c7859ebbb
Brennan <mhszeto.2019@sis.smu.edu.sg>
2021-10-11 19:26:11 +0800

    insert to mongodb flask.py done

--- SPM T5 Project/sample_app.py
+++ SPM T5 Project/sample_app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, jsonify, request,redirect,url_for
+from flask_cors import CORS
 from functools import wraps
 # from flask_pymongo import PyMongo
 from flask_pymongo import pymongo
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from datetime import datetime
+from os import environ
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb://spm_team:spmbestteam@18.136.194.180:27017/")
+spmDatabase = myclient["spm_aio_db"]
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('mongodb://spm_team:spmbestteam@18.136.194.180:27017/') 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 connection = pymongo.MongoClient(
     "18.136.194.180",
     username="spm_team",
@@ -16,14 +25,17 @@ connection = pymongo.MongoClient(
 db = connection["spm_aio_db"]
 collection = db["class"]
 
+CORS(app)
+
+
+
+
 
 #display all classes
 @app.route("/all_class",methods = ["GET"])
 def all_class():
     all_class = collection.find() 
-    # return all_class[0]
     return render_template('create_class.html', all_class = all_class)
-
 
 #Select one class to display according to specified id
 @app.route("/each_class/<id>",methods = ["GET"])
@@ -35,20 +47,24 @@ def each_class(id):
 #create new class
 @app.route("/create_class",methods = ["POST"])
 def create_class():
+    #extracting filled in forms
     json = request.json
-    course_id = request.form['course_id']
-    class_id = request.form['class_id']
-    trainer_name = request.form['trainer_name']
-    trainer_username = request.form['trainer_username']
-    admin_username = request.form['admin_username']
-    enrolment_open_date = datetime(2021, 10, 25, 0, 0, 0, 0)
-    enrolment_close_date = datetime(2021, 11, 5, 0, 0, 0, 0)
-    start_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
-    end_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
-    class_size = request.form['class_size']
-    class_available_slots = request.form['class_available_slots']
 
-    if trainer_name and trainer_username and admin_username and enrolment_open_date and enrolment_close_date and start_date_time and end_date_time and class_size and class_available_slots and request.method == 'POST':
+
+
+    #inserting to MongoDB
+    if request.method == 'POST':
+        course_id = request.form['course_id']
+        class_id = request.form['class_id']
+        trainer_name = request.form['trainer_name']
+        trainer_username = request.form['trainer_username']
+        admin_username = request.form['admin_username']
+        enrolment_open_date = datetime(2021, 10, 25, 0, 0, 0, 0)
+        enrolment_close_date = datetime(2021, 11, 5, 0, 0, 0, 0)
+        start_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
+        end_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
+        class_size = request.form['class_size']
+        class_available_slots = request.form['class_available_slots']
         id = collection.insert(
             {
                 "_id": {"course_id": course_id, "class_id": class_id},
