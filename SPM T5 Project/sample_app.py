b389c7c543d5aaf91a8c8e8da9c8e66f90451d01
Brennan <mhszeto.2019@sis.smu.edu.sg>
2021-10-11 14:59:09 +0800

    save edits

--- SPM T5 Project/sample_app.py
+++ SPM T5 Project/sample_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request,redirect,url_for
 from functools import wraps
 # from flask_pymongo import PyMongo
 from flask_pymongo import pymongo
@@ -31,7 +31,7 @@ collection = db["class"]
 def all_class():
     all_class = collection.find() 
     resp = dumps(all_class)
-    return resp
+    return render_template('create_class.html')
 
 
 #Select one class to display according to specified id
@@ -45,17 +45,17 @@ def each_class(id):
 @app.route("/create_class",methods = ["POST"])
 def create_class():
     json = request.json
-    # course_id = "CR102"
-    # class_id = "CL4"
-    # trainer_name = "Chua ah huat"
-    # trainer_username = "JaneBestEngineer"
-    # admin_username = "LarryThePaperChaser"
-    # enrolment_open_date = datetime(2021, 10, 25, 0, 0, 0, 0)
-    # enrolment_close_date = datetime(2021, 11, 5, 0, 0, 0, 0)
-    # start_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
-    # end_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
-    # class_size = 50
-    # class_available_slots = 20
+    course_id = request.form['course_id']
+    class_id = request.form['class_id']
+    trainer_name = request.form['trainer_name']
+    trainer_username = request.form['trainer_username']
+    admin_username = request.form['admin_username']
+    enrolment_open_date = datetime(2021, 10, 25, 0, 0, 0, 0)
+    enrolment_close_date = datetime(2021, 11, 5, 0, 0, 0, 0)
+    start_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
+    end_date_time = datetime(2021, 11, 5, 0, 0, 0, 0)
+    class_size = request.form['class_size']
+    class_available_slots = request.form['class_available_slots']
 
     if trainer_name and trainer_username and admin_username and enrolment_open_date and enrolment_close_date and start_date_time and end_date_time and class_size and class_available_slots and request.method == 'POST':
         id = collection.insert(
@@ -74,7 +74,7 @@ def create_class():
         )
         resp = jsonify("class added successfully")
         resp.status_code = 200
-        return resp
+        return redirect(url_for('all_class'))
 
     else:
         return not_found()
