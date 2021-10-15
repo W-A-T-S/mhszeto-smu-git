3f3fe1c83883cd2e9a0039bd59b58bd7017bd306
Brennan <mhszeto.2019@sis.smu.edu.sg>
2021-10-16 02:08:58 +0800

    test_course tdd

---
+++ SPM T5 Project/test/test_course.py
@@ -0,0 +1,15 @@
+import unittest
+from courseDAO import Course;
+
+class testCourse(unittest.TestCase):
+    def test_get_courses(self):
+        course = Course("CR109",
+                "Super engineering",
+                "How to create a time machine",
+                "yes",
+               'LarryThepaperchaser',
+                ['CR101','CR102']
+        )
+        course_id = course._course_id()
+        self.assertEqual(course_id,'CR109')
+        self.assertEqual(course_id,'CR109')
