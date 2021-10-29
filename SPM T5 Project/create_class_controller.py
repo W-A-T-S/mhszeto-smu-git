a778322bd64164eeecc66e5fe701235c5fdc8044
Brennan <mhszeto.2019@sis.smu.edu.sg>
2021-10-29 15:31:09 +0800

    Update create_class_controller.py
    Changed ip addresses to remote server

--- SPM T5 Project/create_class_controller.py
+++ SPM T5 Project/create_class_controller.py
@@ -44,8 +44,8 @@ def create_class():
 
     classDAO.insert_one(one_class_object)
 
-    return redirect(f'http://127.0.0.1:5000/classes/{request.form["course_id"]}')
 
+    return redirect(f'http://18.234.140.174:5000/classes/{request.form["course_id"]}')
     # except:
     #     return (
     #         jsonify({"code": 400, "msg": "Failed Creating class and course!"}),
@@ -54,4 +54,4 @@ def create_class():
 
 
 if __name__ == "__main__":
-    app.run(port="5003", debug=True)
+    app.run(host= "0.0.0.0",port="5003", debug=True)
