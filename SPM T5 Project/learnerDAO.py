d8516c13ac1bc5e8b83bd232a0b770f00723682e
izzkhairable <izzkhair@gmail.com>
2021-11-02 22:40:37 +0800

    Completed feature learner take quiz

--- SPM T5 Project/learnerDAO.py
+++ SPM T5 Project/learnerDAO.py
@@ -1,6 +1,7 @@
 import pymongo
 from learnerDomain import Learner
 
+
 class LearnerDAO:
     def __init__(self):
         connection = pymongo.MongoClient(
@@ -14,7 +15,9 @@ class LearnerDAO:
         self._collection = db["learner"]
 
     def find_all(self):
-        many_learner = list(self._collection.find(dict({"is_trainer": False, "is_admin": False})))
+        many_learner = list(
+            self._collection.find(dict({"is_trainer": False, "is_admin": False}))
+        )
         many_learner_objects = []
         for one_learner in many_learner:
             one_learner_object = Learner(
@@ -26,15 +29,26 @@ class LearnerDAO:
         return many_learner_objects
 
     def find_one(self, learner_username):
-        #mongodb function - find_one
-        #one_learner is dictionary retrieved from monogodb 
-        one_learner = self._collection.find_one(dict({"is_trainer": False, 
-                                        "is_admin": False, 
-                                        "_id.username": learner_username }))
+        # mongodb function - find_one
+        # one_learner is dictionary retrieved from monogodb
+        one_learner = self._collection.find_one(
+            dict(
+                {
+                    "is_trainer": False,
+                    "is_admin": False,
+                    "_id.username": learner_username,
+                }
+            )
+        )
 
-        learner = Learner(username=one_learner["_id"]["username"], 
-                         name=one_learner["name"],
-                         current_designation=one_learner["current_designation"])
+        learner = Learner(
+            username=one_learner["_id"]["username"],
+            name=one_learner["name"],
+            current_designation=one_learner["current_designation"],
+        )
 
         return learner
 
+    def update_one(self, queryIdentifier, queryValues):
+        res = self._collection.update_one(queryIdentifier, queryValues)
+        return res
--- SPM T5 Project/learner_attempt_quiz_controller.py
+++ SPM T5 Project/learner_attempt_quiz_controller.py
@@ -1,24 +1,124 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, request, jsonify, redirect
 from courseDAO import CourseDAO
 from classDAO import ClassDAO
 from trainerDAO import TrainerDAO
 from learnerDAO import LearnerDAO
 from materialCheckDAO import MaterialCheckDAO
 from quizAttemptDAO import QuizAttemptDAO
+from quizAttemptDomain import QuizAttempt
 from lessonDAO import LessonDAO
 from materialDAO import MaterialDAO
 from learnerAssignOrEnrolDAO import LearnerAssignOrEnrolDAO
+from questionDAO import QuestionDAO
+from quizDAO import QuizDAO
+import json
 
-app = Flask(__name__,template_folder ="./Templates" )
+app = Flask(__name__, template_folder="./Templates")
+
 
 @app.route("/view_quiz/<string:class_id>/<string:course_id>/<string:lesson_id>")
-def view_quiz(class_id,course_id,lesson_id):
-    return render_template("takequiz.html")
+def view_quiz(class_id, course_id, lesson_id):
+    quizDao = QuizDAO()
+    questionDAO = QuestionDAO()
+
+    # class_id=one_quiz["_id"]["class_id"],
+    #      course_id=one_quiz["_id"]["course_id"],
+    #       lesson_id=one_quiz["_id"]["lesson_id"],
+    #             title=one_quiz["title"],
+    #             description=one_quiz["description"],
+    #             time_limit=one_quiz["time_limit"],
+    #             passing_percentage=one_quiz["passing_percentage"],
+    #             is_final=one_quiz["is_final"]
+
+    one_quiz = quizDao.find_one(
+        course_id=course_id, class_id=class_id, lesson_id=lesson_id
+    )
+    all_questions = questionDAO.find_all_question_in_quiz(
+        course_id=course_id, class_id=class_id, lesson_id=lesson_id
+    )
+    quiz_dict = {
+        "class_id": class_id,
+        "course_id": course_id,
+        "lesson_id": lesson_id,
+        "title": one_quiz.get_title(),
+        "description": one_quiz.get_description(),
+        "time_limit": one_quiz.get_time_limit(),
+        "passing_percentage": one_quiz.get_passing_percentage(),
+        "is_final": one_quiz.get_is_final(),
+        "question_list": [],
+    }
+
+    for one_question in all_questions:
+        quiz_dict["question_list"].append(
+            {
+                "question": one_question.get_question(),
+                "choices": one_question.get_options(),
+                "correctAnswer": one_question.get_answer(),
+            }
+        )
+
+    return render_template("takequiz.html", quiz=json.dumps(quiz_dict))
 
 
+@app.route("/view_quiz/<string:class_id>/<string:course_id>")
+def view_final_quiz(class_id, course_id):
+    quizDao = QuizDAO()
+    questionDAO = QuestionDAO()
 
-# @app.route("attempt_quiz/<string:class_id>/<string:course_id>/<string:lesson_id>")
-# def 
+    one_quiz = quizDao.find_one(course_id=course_id, class_id=class_id, lesson_id=None)
+    all_questions = questionDAO.find_all_question_in_quiz(
+        course_id=course_id, class_id=class_id, lesson_id=None
+    )
+    quiz_dict = {
+        "class_id": class_id,
+        "course_id": course_id,
+        "lesson_id": None,
+        "title": one_quiz.get_title(),
+        "description": one_quiz.get_description(),
+        "time_limit": one_quiz.get_time_limit(),
+        "passing_percentage": one_quiz.get_passing_percentage(),
+        "is_final": one_quiz.get_is_final(),
+        "question_list": [],
+    }
+
+    for one_question in all_questions:
+        quiz_dict["question_list"].append(
+            {
+                "question": one_question.get_question(),
+                "choices": one_question.get_options(),
+                "correctAnswer": one_question.get_answer(),
+            }
+        )
+
+    return render_template("takequiz.html", quiz=json.dumps(quiz_dict))
+
+
+@app.route("/update_attempt", methods=["POST"])
+def update_attempt():
+    quizAttemptDAO = QuizAttemptDAO()
+    one_quiz_attempt_obj = QuizAttempt(
+        quiz_attempt_id=request.json["quiz_attempt_id"],
+        lesson_id=request.json["lesson_id"],
+        class_id=request.json["class_id"],
+        course_id=request.json["course_id"],
+        learner_username=request.json["learner_username"],
+        marks_awarded=request.json["marks_awarded"],
+        is_passed=request.json["is_passed"],
+    )
+
+    quizAttemptDAO.insert_one(one_quiz_attempt_obj)
+
+    if request.json["lesson_id"] == None:
+        learnerAssignOrEnrolDAO = LearnerAssignOrEnrolDAO()
+        update_learner_status_class_complete = learnerAssignOrEnrolDAO.update_one(
+            queryIdentifier={
+                "_id.course_id": request.json["course_id"],
+                "_id.class_id": request.json["class_id"],
+                "_id.learner_username": request.json["learner_username"],
+            },
+            queryValues={"$set": {"is_completed": True}},
+        )
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5008, debug=True)
+    app.run(host="0.0.0.0", port=5008, debug=True)
